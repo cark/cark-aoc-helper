62ae4b5ad919f5c98230ba8aee188ec388f24f94
Cark <carkhy@gmail.com>
2022-12-06 17:05:12 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+fn run_with_timing<Solution>(f: impl Fn() -> Solution) -> (Solution, u128)
+where
+    Solution: std::fmt::Display,
+{
+    let start_time = std::time::Instant::now();
+    let result = f();
+    let duration = start_time.elapsed().as_micros();
+    (result, duration)
+}
+
+pub fn exec_printing_duration<Value>(topic: &str, f: impl Fn() -> Value) -> Value
+where
+    Value: std::fmt::Display,
+{
+    let (result, duration) = run_with_timing(f);
+    println!("{topic}: {duration} µs.");
+    result
+}
+
+pub fn exec_and_print<Value>(topic: &str, f: impl Fn() -> Value)
+where
+    Value: std::fmt::Display,
+{
+    let (result, duration) = run_with_timing(f);
+    println!("{topic}: {result} in {duration} µs.");
+}
+
+#[cfg(test)]
+mod tests {
+    // No tests, we never make any mistake ... right ?
+}
