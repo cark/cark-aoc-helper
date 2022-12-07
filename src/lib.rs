4a013dfa107cc88c034e42f2a4ed44fa407cc0df
Cark <carkhy@gmail.com>
2022-12-07 09:59:59 +0100

    relaced display bound where superfluous

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,11 @@
-fn run_with_timing<Solution>(f: impl Fn() -> Solution) -> (Solution, u128)
-where
-    Solution: std::fmt::Display,
-{
+fn run_with_timing<Solution>(f: impl Fn() -> Solution) -> (Solution, u128) {
     let start_time = std::time::Instant::now();
     let result = f();
     let duration = start_time.elapsed().as_micros();
     (result, duration)
 }
 
-pub fn exec_printing_duration<Value>(topic: &str, f: impl Fn() -> Value) -> Value
-where
-    Value: std::fmt::Display,
-{
+pub fn exec_printing_duration<Value>(topic: &str, f: impl Fn() -> Value) -> Value {
     let (result, duration) = run_with_timing(f);
     println!("{topic}: {duration} µs.");
     result
