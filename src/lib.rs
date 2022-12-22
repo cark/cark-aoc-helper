ee8035823d25e017062cd447d10b012d1bb0b1e4
Cark <carkhy@gmail.com>
2022-12-22 18:15:28 +0100

    mutable closure

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,17 @@
-fn run_with_timing<Solution>(f: impl Fn() -> Solution) -> (Solution, u128) {
+fn run_with_timing<Solution>(mut f: impl FnMut() -> Solution) -> (Solution, u128) {
     let start_time = std::time::Instant::now();
     let result = f();
     let duration = start_time.elapsed().as_micros();
     (result, duration)
 }
 
-pub fn exec_printing_duration<Value>(topic: &str, f: impl Fn() -> Value) -> Value {
+pub fn exec_printing_duration<Value>(topic: &str, f: impl FnMut() -> Value) -> Value {
     let (result, duration) = run_with_timing(f);
     println!("{topic}: {duration} µs.");
     result
 }
 
-pub fn exec_and_print<Value>(topic: &str, f: impl Fn() -> Value)
+pub fn exec_and_print<Value>(topic: &str, f: impl FnMut() -> Value)
 where
     Value: std::fmt::Display,
 {
